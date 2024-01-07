893a87b69fa6acdbcbfebb96fde249edeb711fb9
ASHIL GEORGE <ashilgeorgepg7736@gmail.com>
2024-01-07 22:34:08 +0530

    commiting the code

---
+++ task/Models/CollageRepository.cs
@@ -0,0 +1,43 @@
+﻿namespace task.Models
+
+{
+
+    public class CollageRepository
+
+    {
+
+        public static List<Student> Students { get; set; } = new List<Student>()
+
+            {
+
+              new Student
+
+              {
+
+                  Id = 1,
+
+                  StudentName = "Jesus",
+
+                  Email = "studentemail1@gmail.com"
+
+              },
+
+              new Student
+
+              {
+
+                  Id = 2,
+
+                  StudentName = "Mary",
+
+                  Email = "studentemail2@gmail.com",
+
+                  Address = "Tvm , INDIA"
+
+              }
+
+            };
+
+    }
+
+}
